1761866e06316a2bd6725131e95b3aa5d36aad8f
leyuanL <74730324+leyuanL@users.noreply.github.com>
2021-06-28 19:10:17 +0200

    框架

--- libs/script.js
+++ libs/script.js
@@ -1,7 +1,13 @@
 
 $(document).ready(function() {
+  $(".CV").hide();
+  $(".Contact").hide();
+  $(".item").show();
+  $("#workarrow").show();
+  $("#conarrow").hide();
+  $("#cvarrow").hide();
 
-  $('.header').ready(function(){
+  $(".header").ready(function(){
     var timer = !1;
   _Ticker = $(".TickerNews").newsTicker();
   _Ticker.on("mouseenter",function(){
@@ -16,5 +22,39 @@ $(document).ready(function() {
     this.startTicker();
   });
 });
+
+$("div.item").hover(function(){$(this).css("color","#000000")},
+function(){$(this).css("color","#AAAAAA")});
+
+$("div.bt").hover(function(){$(this).css("color","#000000")},
+function(){$(this).css("color","#AAAAAA")});
+
+$("#cv").click(function(){
+    $(".item").fadeOut();
+    $("#workarrow").fadeOut();
+    $(".Contact").fadeOut();
+    $("#conarrow").fadeOut();
+    $(".CV").fadeIn();
+    $("#cvarrow").fadeIn();
+  });
+
+  $("#works").click(function(){
+      $(".CV").fadeOut();
+      $("#cvarrow").fadeOut();
+      $(".Contact").fadeOut();
+      $("#conarrow").fadeOut();
+      $(".item").fadeIn();
+      $("#workarrow").fadeIn();
+    });
+
+    $("#contact").click(function(){
+        $(".item").fadeOut();
+        $(".CV").fadeOut();
+        $("#cvarrow").fadeOut();
+        $("#workarrow").fadeOut();
+        $(".Contact").fadeIn();
+        $("#conarrow").fadeIn();
+      });
+
 
 });
